6275fd98c886f89c336626e1b1cd80c3c9f993f4
rofazhasan <139000822+rofazhasan@users.noreply.github.com>
2025-04-10 01:51:06 +0600

    Update NotReceived.tsx

--- src/screens/NotReceived.tsx
+++ src/screens/NotReceived.tsx
@@ -1,41 +1,37 @@
 import React, { JSX } from "react";
-
 import {
   StyleSheet,
   View,
   ScrollView,
 } from "react-native";
-
 import { StackScreenProps } from "@react-navigation/stack";
 import { StackParamList } from "../types/Navigator";
-
 import { TransactionsContext } from "../providers/Transactions";
 
 type Props = StackScreenProps<StackParamList, "NotReceived">;
 
-export default function NotReceivedScreen({ navigation }: Props): JSX.Element {
-  const { NotReceived } = React.useContext(TransactionsContext);
+const OutstandingIncomingsScreen: React.FC<Props> = ({ navigation }) => {
+  const { TabulateOutstandingIncomings } = React.useContext(TransactionsContext);
 
-  return(
-    <ScrollView style={[ styles.container ]}>
-      <View style={[ styles.transactionsContainer ]}>
-        <NotReceived />
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.transactionsContainer}>
+        <TabulateOutstandingIncomings />
       </View>
     </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
     backgroundColor: "#ffffff",
   },
-
   transactionsContainer: {
     justifyContent: "center",
-
-    marginVertical: 45,
-    marginHorizontal: 20,
-  }
+    marginVertical: 35, // Subtly adjusted vertical margin
+    marginHorizontal: 18, // Subtly adjusted horizontal margin
+  },
 });
+
+export default OutstandingIncomingsScreen;
