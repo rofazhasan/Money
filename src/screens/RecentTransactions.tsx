63029e20da90c446a033c40f41fead2163d3fcf7
rofazhasan <139000822+rofazhasan@users.noreply.github.com>
2025-04-10 01:58:18 +0600

    Update RecentTransactions.tsx

--- src/screens/RecentTransactions.tsx
+++ src/screens/RecentTransactions.tsx
@@ -1,41 +1,37 @@
 import React, { JSX } from "react";
-
 import {
   StyleSheet,
   View,
   ScrollView,
 } from "react-native";
-
 import { StackScreenProps } from "@react-navigation/stack";
 import { StackParamList } from "../types/Navigator";
-
 import { TransactionsContext } from "../providers/Transactions";
 
 type Props = StackScreenProps<StackParamList, "RecentTransactions">;
 
-export default function RecentTransactionsScreen({ navigation }: Props): JSX.Element {
-  const { RecentTransactions } = React.useContext(TransactionsContext);
+const RecentFinancialMovementsScreen: React.FC<Props> = ({ navigation }) => {
+  const { ChronicleRecentTransactions } = React.useContext(TransactionsContext);
 
-  return(
-    <ScrollView style={[ styles.container ]}>
-      <View style={[ styles.transactionsContainer ]}>
-        <RecentTransactions  />
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.transactionsContainer}>
+        <ChronicleRecentTransactions />
       </View>
     </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
     backgroundColor: "#ffffff",
   },
-
   transactionsContainer: {
     justifyContent: "center",
-
-    marginVertical: 45,
-    marginHorizontal: 20,
-  }
+    marginVertical: 35, // Subtly refined vertical margin
+    marginHorizontal: 18, // Subtly refined horizontal margin
+  },
 });
+
+export default RecentFinancialMovementsScreen;
