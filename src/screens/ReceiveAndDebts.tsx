575e746c7da9ec9dd137ec57ac87d3247542b1d3
rofazhasan <139000822+rofazhasan@users.noreply.github.com>
2025-04-10 01:55:10 +0600

    Update ReceiveAndDebts.tsx

--- src/screens/ReceiveAndDebts.tsx
+++ src/screens/ReceiveAndDebts.tsx
@@ -1,41 +1,37 @@
 import React, { JSX } from "react";
-
 import {
   StyleSheet,
   View,
   ScrollView,
 } from "react-native";
-
 import { StackScreenProps } from "@react-navigation/stack";
 import { StackParamList } from "../types/Navigator";
-
 import { TransactionsContext } from "../providers/Transactions";
 
-type Props = StackScreenProps<StackParamList, "ReceiveAndDebts">; // Corrected here
+type Props = StackScreenProps<StackParamList, "ReceiveAndDebts">;
 
-export default function ReceiveAndDebtsScreen({ navigation }: Props): JSX.Element {
-  const { ReceiveAndDebts } = React.useContext(TransactionsContext);
+const OutstandingBalancesScreen: React.FC<Props> = ({ navigation }) => {
+  const { CatalogueReceivablesAndPayables } = React.useContext(TransactionsContext);
 
-  return(
-    <ScrollView style={[ styles.container ]}>
-      <View style={[ styles.transactionsContainer ]}>
-        <ReceiveAndDebts />
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.transactionsContainer}>
+        <CatalogueReceivablesAndPayables />
       </View>
     </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
     backgroundColor: "#ffffff",
   },
-
   transactionsContainer: {
     justifyContent: "center",
-
-    marginVertical: 45,
-    marginHorizontal: 20,
-  }
+    marginVertical: 35, // Elegantly adjusted vertical margin
+    marginHorizontal: 18, // Elegantly adjusted horizontal margin
+  },
 });
+
+export default OutstandingBalancesScreen;
