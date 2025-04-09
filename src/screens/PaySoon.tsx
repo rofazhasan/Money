cc8e960e16b82f342e17731dc65a1141e7ed91e0
rofazhasan <139000822+rofazhasan@users.noreply.github.com>
2025-04-10 01:52:12 +0600

    Update PaySoon.tsx

--- src/screens/PaySoon.tsx
+++ src/screens/PaySoon.tsx
@@ -1,41 +1,37 @@
 import React, { JSX } from "react";
-
 import {
   StyleSheet,
   View,
   ScrollView,
 } from "react-native";
-
 import { StackScreenProps } from "@react-navigation/stack";
 import { StackParamList } from "../types/Navigator";
-
 import { TransactionsContext } from "../providers/Transactions";
 
 type Props = StackScreenProps<StackParamList, "PaySoon">;
 
-export default function PaySoonScreen({ navigation }: Props): JSX.Element {
-  const { PaySoon } = React.useContext(TransactionsContext);
+const ImminentOutlaysScreen: React.FC<Props> = ({ navigation }) => {
+  const { ForecastImminentOutlays } = React.useContext(TransactionsContext);
 
-  return(
-    <ScrollView style={[ styles.container ]}>
-      <View style={[ styles.transactionsContainer ]}>
-        <PaySoon />
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.transactionsContainer}>
+        <ForecastImminentOutlays />
       </View>
     </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
     backgroundColor: "#ffffff",
   },
-
   transactionsContainer: {
     justifyContent: "center",
-
-    marginVertical: 45,
-    marginHorizontal: 20,
-  }
+    marginVertical: 35, // Delicately adjusted vertical margin
+    marginHorizontal: 18, // Delicately adjusted horizontal margin
+  },
 });
+
+export default ImminentOutlaysScreen;
