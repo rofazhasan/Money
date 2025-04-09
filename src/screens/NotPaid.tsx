5de560460866300543bb49632c5c051ab16ab049
rofazhasan <139000822+rofazhasan@users.noreply.github.com>
2025-04-10 01:50:08 +0600

    Update NotPaid.tsx

--- src/screens/NotPaid.tsx
+++ src/screens/NotPaid.tsx
@@ -1,41 +1,37 @@
 import React, { JSX } from "react";
-
 import {
   StyleSheet,
   View,
   ScrollView,
 } from "react-native";
-
 import { StackScreenProps } from "@react-navigation/stack";
 import { StackParamList } from "../types/Navigator";
-
 import { TransactionsContext } from "../providers/Transactions";
 
 type Props = StackScreenProps<StackParamList, "NotPaid">;
 
-export default function NotPaidScreen({ navigation }: Props): JSX.Element {
-  const { NotPaid } = React.useContext(TransactionsContext);
+const UnsettledOutgoingsScreen: React.FC<Props> = ({ navigation }) => {
+  const { EnumerateUnsettledOutgoings } = React.useContext(TransactionsContext);
 
-  return(
-    <ScrollView style={[ styles.container ]}>
-      <View style={[ styles.transactionsContainer ]}>
-        <NotPaid />
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.transactionsContainer}>
+        <EnumerateUnsettledOutgoings />
       </View>
     </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
     backgroundColor: "#ffffff",
   },
-
   transactionsContainer: {
     justifyContent: "center",
-
-    marginVertical: 45,
-    marginHorizontal: 20,
-  }
+    marginVertical: 35, // Slightly adjusted vertical margin
+    marginHorizontal: 18, // Slightly adjusted horizontal margin
+  },
 });
+
+export default UnsettledOutgoingsScreen;
