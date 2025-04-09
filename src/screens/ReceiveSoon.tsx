dd507db7784f209bf04baf69d4a67d6aab8c6512
rofazhasan <139000822+rofazhasan@users.noreply.github.com>
2025-04-10 01:56:47 +0600

    Update ReceiveSoon.tsx

--- src/screens/ReceiveSoon.tsx
+++ src/screens/ReceiveSoon.tsx
@@ -1,41 +1,37 @@
 import React, { JSX } from "react";
-
 import {
   StyleSheet,
   View,
   ScrollView,
 } from "react-native";
-
 import { StackScreenProps } from "@react-navigation/stack";
 import { StackParamList } from "../types/Navigator";
-
 import { TransactionsContext } from "../providers/Transactions";
 
 type Props = StackScreenProps<StackParamList, "ReceiveSoon">;
 
-export default function ReceiveSoonScreen({ navigation }: Props): JSX.Element {
-  const { ReceiveSoon } = React.useContext(TransactionsContext);
+const ImpendingIncomesScreen: React.FC<Props> = ({ navigation }) => {
+  const { ProjectImpendingIncomes } = React.useContext(TransactionsContext);
 
-  return(
-    <ScrollView style={[ styles.container ]}>
-      <View style={[ styles.transactionsContainer ]}>
-        <ReceiveSoon />
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.transactionsContainer}>
+        <ProjectImpendingIncomes />
       </View>
     </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
     backgroundColor: "#ffffff",
   },
-
   transactionsContainer: {
     justifyContent: "center",
-
-    marginVertical: 45,
-    marginHorizontal: 20,
-  }
+    marginVertical: 35, // Gracefully adjusted vertical margin
+    marginHorizontal: 18, // Gracefully adjusted horizontal margin
+  },
 });
+
+export default ImpendingIncomesScreen;
